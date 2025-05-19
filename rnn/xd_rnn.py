5ec01629895dc6d05329239d2937fd716f8a21b0
Hyun Park <hpark@xdworks.co.kr>
2025-05-19 14:36:42 +0900

    dev: RNN 성능이 나오지 않음. todo: method 1 - country_classification_1.ipynb 구현

--- rnn/xd_rnn.py
+++ rnn/xd_rnn.py
@@ -3,15 +3,15 @@ import torch.nn as nn
 
 class XD_RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
-      super(XD_RNN, self).__init__()
+      super().__init__()
 
       self.hidden_size = hidden_size
       self.i2h = torch.nn.Linear(input_size + hidden_size, hidden_size)
       self.h2o = torch.nn.Linear(hidden_size, output_size)
 
     def forward(self, input, hidden):
-        combined = torch.cat(input, hidden);
-        hidden = self.i2h(combined);
+        combined = torch.cat((input, hidden), 1);
+        hidden = torch.tanh(self.i2h(combined));
         output = self.h2o(hidden)
         return output, hidden;
 
